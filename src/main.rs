72ed1b6d9eda9545dc7fbad7c9c13df63d1e6b2e
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2023-02-15 15:51:23 +0100

    Scaffolding laid out. Time for version 1.

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@
 //                 //check if permutation exists
 //                 //if exists add permutation as neighbour
 //             }
-            
+
 //             self.nodes.insert(
 //                 permutation,
 //                 Node {
@@ -85,4 +85,87 @@
 //     println!("Indexing 200000 points: {:?}", start.elapsed());
 // }
 
+fn all_transpositions(mut n: u128) -> impl Iterator<Item = u128> {
+    let mut mask = 1;
+    let mut flips = Vec::new();
+    while n > 0 {
+        flips.push(n ^ mask);
+        mask <<= 1;
+        n >>= 1;
+    }
+    flips.into_iter()
+}
 
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+struct Node {
+    id: u128,
+    data_points: Vec<usize>,
+    neighbours: Vec<usize>,
+}
+
+struct Index {
+    nodes: Vec<Node>,
+    index_map: hashbrown::HashMap<u128, usize>,
+}
+
+impl Index {
+    fn new() -> Self {
+        Index {
+            nodes: Vec::new(),
+            index_map: hashbrown::HashMap::new(),
+        }
+    }
+
+    fn add_permutation(&mut self, node: Node) {
+        let id = node.id;
+        self.nodes.push(node);
+        let index = self.nodes.len() - 1;
+        self.index_map.insert(id, index);
+    }
+
+    fn add_subpermutation(&mut self, subpermutation: u128, data_points:Vec<usize>) {
+        let neighbours: Vec<usize> = Vec::new();
+        let subpermutation_index = self.nodes.len() - 1;
+        for transposition in all_transpositions(subpermutation) {
+            match self.index_map.get(transposition) {
+                Some(index) => {
+                    //if exists add indexes to both
+                    self.nodes[*index].neighbours.push(subpermutation_index);
+                    neighbours.push(*index);
+                }
+                None => continue,
+            }
+        }
+        self.add_permutation(Node {
+            id: subpermutation,
+            data_points: data_points,
+            neighbours: neighbours,
+        });
+    }
+
+    fn add_new_permutation(&mut self, binary: u128, data_point: usize) {
+        //for each sibpermutation:
+
+        //for each translation:
+        self.add_subpermutation(subpermutation);
+
+        //if it exists
+    }
+
+    fn insert(&mut self, point: Vec<T>, data_point: usize) {
+        //icalcualte distance to simplexes
+        let binary = distance_to_simplex(point, &self.simplexes);
+
+        //check if permutation exists
+        match self.index_map.get(binary) {
+            Some(index) => {
+                //if it does add data point to it
+                self.nodes[*index].data_points.push(data_point);
+            }
+            None => {
+                //else add it
+                self.add_new_permutation(binary, data_point);
+            }
+        }
+    }
+}
