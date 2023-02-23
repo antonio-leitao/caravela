79c09ba11975de45c94ea4950d1b665b0cfa2349
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2023-02-23 19:32:51 +0100

    major directory changes

--- src/main.rs
+++ src/main.rs
@@ -1,189 +1,41 @@
-// use std::time::Instant;
 
-// fn main() {
-//     let value: u128 = 0x1234_5678_9ABC_DEF0_1111_2222_3333_4444;
-//     let random_hash = select_32_bits();
-//     let start = Instant::now();
-//     for _ in 0..1_000_000 {
-//         _=random_hash(value)
-//     }
-//     println!("{:?}",start.elapsed());
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
-// }
+fn random_binary_with_ones(num_ones: u32, num_bits: u32) -> u64 {
+    assert!(num_bits == 8 || num_bits == 16 || num_bits == 32 || num_bits == 64);
+    assert!(num_ones <= num_bits);
 
-use std::time::{Duration, Instant};
-mod indexes;
-mod utils;
-use csv::WriterBuilder;
-use std::error::Error;
-use std::fs::File;
-use std::io::Write;
+    let mut rng = thread_rng();
+    let mut indices: Vec<usize> = (0..num_bits as usize).collect();
+    indices.truncate(num_ones as usize);
+    indices.shuffle(&mut rng);
 
-fn permutation_to_u128(vector: &[usize]) -> u128 {
-    let mut packed: u128 = 0;
-    for i in 0..vector.len() {
-        for j in i + 1..vector.len() {
-            packed <<= 1;
-            if vector[i] > vector[j] {
-                packed |= 1;
-            }
-        }
+    let mut bits: u64 = 0;
+    for i in indices {
+        bits |= 1 << i;
     }
-    packed
+    bits
 }
 
-fn run_metrics64(n_samples: usize, n_queries: usize, n_hashes: usize) -> (f32, f32) {
-    let mut index: indexes::Index64::Index64 = indexes::Index64::Index64::new(n_hashes);
-    let permutations = utils::random_permutations(n_samples, 16);
-
-    for (data_point, permutation) in permutations.iter().enumerate() {
-        index.insert(permutation_to_u128(&permutation), data_point);
-    }
-
-    let queries = utils::random_permutations(n_queries, 16);
-    let mut total_time = Duration::new(0, 0);
-    let mut count: u32 = 0;
-
-    for permutation in queries.iter() {
-        let start_time = Instant::now();
-        let candidates = index.query(permutation_to_u128(&permutation));
-
-        let elapsed_time = start_time.elapsed();
-        total_time += elapsed_time;
-        count += candidates.len() as u32;
-    }
-    let avg_time = total_time.as_millis() as f32 / n_queries as f32;
-    let avg_pool = count as f32 / n_queries as f32;
-    (avg_time, avg_pool)
+#[test]
+fn test_correct_number_of_bits() {
+    let num_ones = 8;
+    let num_bits = 32;
+    let random_binary = random_binary_with_ones(num_ones, num_bits);
+    let count_ones = random_binary.count_ones();
+    assert_eq!(count_ones, num_ones);
 }
 
-fn run_metrics32(n_samples: usize, n_queries: usize, n_hashes: usize) -> (f32, f32) {
-    let mut index: indexes::Index32::Index32 = indexes::Index32::Index32::new(n_hashes);
-    let permutations = utils::random_permutations(n_samples, 16);
-
-    for (data_point, permutation) in permutations.iter().enumerate() {
-        index.insert(permutation_to_u128(&permutation), data_point);
-    }
-
-    let queries = utils::random_permutations(n_queries, 16);
-    let mut total_time = Duration::new(0, 0);
-    let mut count: u32 = 0;
-
-    for permutation in queries.iter() {
-        let start_time = Instant::now();
-        let candidates = index.query(permutation_to_u128(&permutation));
-
-        let elapsed_time = start_time.elapsed();
-        total_time += elapsed_time;
-        count += candidates.len() as u32;
-    }
-    let avg_time = total_time.as_millis() as f32 / n_queries as f32;
-    let avg_pool = count as f32 / n_queries as f32;
-    (avg_time, avg_pool)
+#[test]
+fn test_random_bit_selection() {
+    let num_ones = 8;
+    let num_bits = 32;
+    let first_random = random_binary_with_ones(num_ones, num_bits);
+    let second_random = random_binary_with_ones(num_ones, num_bits);
+    assert_ne!(first_random, second_random, "Bit selection is not random");
 }
 
-fn run_metrics16(n_samples: usize, n_queries: usize, n_hashes: usize) -> (f32, f32) {
-    let mut index: indexes::Index16::Index16 = indexes::Index16::Index16::new(n_hashes);
-    let permutations = utils::random_permutations(n_samples, 16);
-
-    for (data_point, permutation) in permutations.iter().enumerate() {
-        index.insert(permutation_to_u128(&permutation), data_point);
-    }
-
-    let queries = utils::random_permutations(n_queries, 16);
-    let mut total_time = Duration::new(0, 0);
-    let mut count: u32 = 0;
-
-    for permutation in queries.iter() {
-        let start_time = Instant::now();
-        let candidates = index.query(permutation_to_u128(&permutation));
-
-        let elapsed_time = start_time.elapsed();
-        total_time += elapsed_time;
-        count += candidates.len() as u32;
-    }
-    let avg_time = total_time.as_millis() as f32 / n_queries as f32;
-    let avg_pool = count as f32 / n_queries as f32;
-    (avg_time, avg_pool)
-}
-
-fn run_metrics8(n_samples: usize, n_queries: usize, n_hashes: usize) -> (f32, f32) {
-    let mut index: indexes::Index8::Index8 = indexes::Index8::Index8::new(n_hashes);
-    let permutations = utils::random_permutations(n_samples, 16);
-
-    for (data_point, permutation) in permutations.iter().enumerate() {
-        index.insert(permutation_to_u128(&permutation), data_point);
-    }
-
-    let queries = utils::random_permutations(n_queries, 16);
-    let mut total_time = Duration::new(0, 0);
-    let mut count: u32 = 0;
-
-    for permutation in queries.iter() {
-        let start_time = Instant::now();
-        let candidates = index.query(permutation_to_u128(&permutation));
-
-        let elapsed_time = start_time.elapsed();
-        total_time += elapsed_time;
-        count += candidates.len() as u32;
-    }
-    let avg_time = total_time.as_millis() as f32 / n_queries as f32;
-    let avg_pool = count as f32 / n_queries as f32;
-    (avg_time, avg_pool)
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut writer = WriterBuilder::new()
-        .has_headers(false)
-        .from_path("output.csv")?;
-
-    writer.write_record(&["indextype", "n_samples", "time", "pool", "n_hashes"])?;
-
-    let n_queries = 1_000;
-    for n_samples in [
-        1_000, 2_000, 5_000, 10_000, 20_000, 50_000, 100_000, 200_000,
-    ] {
-        for n_hashes in [1, 2, 5, 10, 20, 50, 100] {
-            let (time, pool) = run_metrics64(n_samples, n_queries, n_hashes);
-            writer.write_record(&[
-                "64",
-                &n_samples.to_string(),
-                &time.to_string(),
-                &pool.to_string(),
-                &n_hashes.to_string(),
-            ])?;
-
-            let (time, pool) = run_metrics32(n_samples, n_queries, n_hashes);
-            writer.write_record(&[
-                "32",
-                &n_samples.to_string(),
-                &time.to_string(),
-                &pool.to_string(),
-                &n_hashes.to_string(),
-            ])?;
-
-            let (time, pool) = run_metrics16(n_samples, n_queries, n_hashes);
-            writer.write_record(&[
-                "16",
-                &n_samples.to_string(),
-                &time.to_string(),
-                &pool.to_string(),
-                &n_hashes.to_string(),
-            ])?;
-
-            let (time, pool) = run_metrics8(n_samples, n_queries, n_hashes);
-            writer.write_record(&[
-                "8",
-                &n_samples.to_string(),
-                &time.to_string(),
-                &pool.to_string(),
-                &n_hashes.to_string(),
-            ])?;
-        }
-        println!{"finished n_samples {:?}", n_samples};
-    }
-
-    writer.flush()?;
-
-    Ok(())
+fn main(){
+    println!("this is main")
 }
